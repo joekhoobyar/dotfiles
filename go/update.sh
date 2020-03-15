52a8686617f99dc957dea32c8f4a4a8c08732c20
Joe Khoobyar <fourheads@gmail.com>
2020-03-15 15:25:17 -0400

    common golang deps

--- go/update.sh
+++ go/update.sh
@@ -12,5 +12,7 @@ source $DOTFILES_REPO/files/.bash/golang.sh
 
 printf "\n> golang update\n"
 go get golang.org/x/tools/cmd/godoc
+go get github.com/golang/dep/cmd/dep
+go get github.com/mitchellh/gox
 
 exit 0
