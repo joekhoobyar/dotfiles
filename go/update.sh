3af489ead2a74f42665a22277de0d3a99e5adb7a
Joe Khoobyar <fourheads@gmail.com>
2021-05-06 20:39:36 -0400

    remove go dep

--- go/update.sh
+++ go/update.sh
@@ -13,7 +13,6 @@ source ~/.bash/golang.sh
 printf "\n> golang update\n"
 go get golang.org/x/tools/cmd/godoc
 go get golang.org/x/tools/gopls
-go get github.com/golang/dep/cmd/dep
 command -v gox >/dev/null || go get github.com/mitchellh/gox
 
 exit 0
