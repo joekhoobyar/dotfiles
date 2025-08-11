63eb19f787fac8e0c3e2ac93005cf1e260e8df8d
Joe Khoobyar <joe.khoobyar+bitbucket@usmobile.com>
2025-08-11 16:03:27 -0400

    go:  add govulncheck

--- go/update.sh
+++ go/update.sh
@@ -15,6 +15,7 @@ printf "\n> golang update\n"
 go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/vuln/cmd/govulncheck@latest
 go install github.com/onsi/ginkgo/v2/ginkgo@latest
 command -v gox >/dev/null || go install github.com/mitchellh/gox@latest
 
